2991aa53a0a4e429fc5fc06a133de5b65655a719
Sumon Ali <sumon01333581557@gmail.com>
2026-06-16 11:33:46 +0600

    Problem solved

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,13 +1,12 @@
 "use server";
 
-const api = "https://fakestoreapi.com/products";
+const api = process.env.NEXT_PUBLIC_API;
 export const getProducts = async () => {
   const res = await fetch(api);
   return res.json();
 };
 
 export const getProductById = async(id)=>{
-  console.log(id)
   const res = await fetch(`${api}/${id}`)
   return res.json()
 }
