2ab011fb7d3bc234f10c30e3dbb8f61cb8d0af15
Sumon Ali <sumon01333581557@gmail.com>
2026-06-16 13:12:46 +0600

    Api realated problem fixed

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,12 +1,21 @@
 "use server";
 
-const api = process.env.NEXT_PUBLIC_API;
+const api = process.env.API_URL;
+
 export const getProducts = async () => {
-  const res = await fetch(`${api}/products`);
-  return res.json();
+  try {
+    const res = await fetch(`${api}/products`);
+    if (!res.ok) {
+      console.log("Fetch failed with status:", res.status);
+    }
+    return res.json();
+  } catch (error) {
+    console.error("API Error details:", error);
+    throw error;
+  }
 };
 
-export const getProductById = async(id)=>{
-  const res = await fetch(`${api}/products/${id}`)
-  return res.json()
-}
+export const getProductById = async (id) => {
+  const res = await fetch(`${api}/products/${id}`);
+  return res.json();
+};
